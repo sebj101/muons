84213cc9fc5fede3e206ffb13479b109c0e32ebe
Seb Jones <jonesseb100@gmail.com>
2017-11-10 15:47:59 +0000

    Added functions to make program much more smoothly.

--- muons.py
+++ muons.py
@@ -5,8 +5,39 @@ from ROOT import TChain, TLorentzVector, TH1F
 data = TChain("mini")
 data.Add("http://opendata.atlas.cern/release/samples/Data/DataMuons.root")
 
+def four_momentum(i_lepton, tree):
+    pt = tree.lep_pt[i_lepton]
+    eta = tree.lep_eta[i_lepton]
+    phi = tree.lep_phi[i_lepton]
+    E = tree.lep_E[i_lepton]
+    p = TLorentzVector()
+    p.SetPtEtaPhiE(pt,eta,phi,E)
+    return p
+
+def leptons_from_event(tree):
+    leptons = []
+    n_lepton = data.lep_n
+    for i in range(n_lepton):
+        p = four_momentum(i, tree)
+        leptons.append(p)
+    return leptons
+
+def pairs_from_particles(particles):
+    pairs = []
+    n_particles = len(particles)
+    for i in range(n_particles-1):
+        for j in range(i+1, n_particles):
+            pair = (particles[i], particles[j])
+            pairs.append(pair)
+    return pairs
+
+def mass_of_pairs(pair):
+    p1 = pair[0]
+    p2 = pair[1]
+    ppair = p1 + p2
+    return ppair.M()
+
 num_events = data.GetEntries()
-print("Number of events = "+str(num_events))
 
 h_mpair = TH1F("mpair", "Invariant mass of lepton pairs", 50, 0, 200)
 
@@ -14,25 +45,13 @@ number_events_to_process = 10000   # Number of events to process
 for i_event in range(number_events_to_process):
     data.GetEntry(i_event)
     n_leptons = data.lep_n
-    if n_leptons == 2:  # Looking for pairs
-        print("Number of leptons = "+str(n_leptons))
-        assert(n_leptons==2)
-        pt1=data.lep_pt[0]
-        eta1 = data.lep_eta[0]
-        phi1 = data.lep_phi[0]
-        e1 = data.lep_E[0]
-        pt2=data.lep_pt[1]
-        eta2 = data.lep_eta[1]
-        phi2 = data.lep_phi[1]
-        e2 = data.lep_E[1]
-        p1 = TLorentzVector()
-        p2 = TLorentzVector()
-        p1.SetPtEtaPhiE(pt1,eta1,phi1,e1)
-        p2.SetPtEtaPhiE(pt2,eta2,phi2,e2)
+    if n_leptons >= 2:  # Looking for pairs
+        leptons = leptons_from_event(data)
+        pairs = pairs_from_particles(leptons)
         # Calculates mass of lepton pair
-        ppair = p1+p2
-        mpair = ppair.M() / 1000 # convert from MeV to GeV
-        h_mpair.Fill(mpair)
+        for pair in pairs:
+            mpair = mass_of_pairs(pair) / 1000 # convert from MeV to GeV
+            h_mpair.Fill(mpair)
 
 h_mpair.Draw()
 print()
